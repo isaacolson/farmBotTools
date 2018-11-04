9de8d89865d48b30ffd62b530cbf78e424031020
4thgear <44248661+4thgear@users.noreply.github.com>
2018-11-03 21:00:34 -0500

    Update find-lines.py

--- find-lines.py
+++ find-lines.py
@@ -4,8 +4,9 @@ imgloc = r""
 img = cv2.imread(imgloc)
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 edges = cv2.Canny(gray,50,150,apertureSize = 3)
+img = cv2.line(img,(814,1609),(814,1679+200),(255,255,255),1)
 
-lines = cv2.HoughLines(edges,1,np.pi/180,200)
+lines = cv2.HoughLines(edges,10,np.pi/180,200)
 for rho,theta in lines[0]:
     a = np.cos(theta)
     b = np.sin(theta)
@@ -17,8 +18,9 @@ for rho,theta in lines[0]:
     y2 = int(y0 - 1000*(a))
 
     cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
-    
+
+
 cv2.imwrite('houghlines_gray.jpg',gray)
 cv2.imwrite('houghlines_canny.jpg',edges)
-cv2.imwrite('houghlines5.jpg',img)
-
+cv2.imwrite('houghlines_write.jpg',img)
+cv2.imwrite('test.jpg',img)
